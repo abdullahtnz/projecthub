816c255bf9c9e1865f9eb427955c6cf4754d2628
Abdullah Novruzlu <novruzluabdullah03@gmail.com>
2026-01-01 21:39:24 +0400

    Added json decoder

--- Backend/AuthDatabase/login.go
+++ Backend/AuthDatabase/login.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -15,6 +16,14 @@ func LoginHandler(pool *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var req UserLoginRequest
+
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
 			return
 		}
 	}
