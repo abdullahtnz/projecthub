c8d0aa646ce4320fa0cf3b0e199b508d6bb64870
Abdullah Novruzlu <novruzluabdullah03@gmail.com>
2026-01-01 21:36:50 +0400

    Added method checker in loginhandler

--- Backend/AuthDatabase/login.go
+++ Backend/AuthDatabase/login.go
@@ -1,6 +1,21 @@
 package database
 
+import (
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 type UserLoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func LoginHandler(pool *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}
 }
