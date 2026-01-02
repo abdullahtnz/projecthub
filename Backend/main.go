10bee9f30cddabe826718876c011c21e1f3e0d17
Abdullah Novruzlu <novruzluabdullah03@gmail.com>
2026-01-02 10:23:10 +0400

    added cors origin enabler for test

--- Backend/main.go
+++ Backend/main.go
@@ -10,6 +10,23 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// handle preflight
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -25,7 +42,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Connection failed:", err)
 	} else {
-		fmt.Print("Succes")
+		fmt.Println("Succes")
 	}
 	defer pool.Close()
 
@@ -33,6 +50,9 @@ func main() {
 
 	mux.HandleFunc("/signup", database.SignupHandler(pool))
 	mux.HandleFunc("/login", database.LoginHandler(pool))
+
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 	// 2026 commit :)
 
